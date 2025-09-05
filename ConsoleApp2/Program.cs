0ac21128fcc4cddeadf467f31b88a7ddea749932
boktikazz <mezeib@kkszki.hu>
2025-09-05 11:25:11 +0200

    Feladat1 is ready.

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -6,10 +6,21 @@ using System.Threading.Tasks;
 
 namespace ConsoleApp2
 {
+
+    public class Szemely 
+    { 
+     public string nev = "Kiss Péter";
+     public int kor = 35;
+    
+    }
     internal class Program
     {
         static void Main(string[] args)
         {
+            Szemely tanulo = new Szemely();
+            Console.WriteLine("Név: " + tanulo.nev);
+            Console.WriteLine("Kor: " + tanulo.kor);
+            Console.ReadKey();
         }
     }
 }
