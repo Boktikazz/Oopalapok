d26d4ede6c628ac211c82470aa02dd3fb52815c3
boktikazz <mezeib@kkszki.hu>
2025-09-12 09:47:00 +0200

    Feladat5 and6,7,8,9 is ready.

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -1,19 +1,46 @@
 ﻿using System;
-
+using System.Collections.Generic;
 
 namespace ConsoleApp2
+
 {
-
-     class Szemely 
+    public class Bankszamla
     {
+        private int egyenleg;
 
-        private string nev;
-        private int kor;
-
-        public Szemely(string Nev)
+        public void Betesz()
         {
-          nev = Nev;
+
         }
+
+        public void Kivesz()
+        {
+
+        }
+    }
+
+    public class Hallgato : Szemely
+    {
+        private string neptunkod;
+
+        public string NeptunKod
+        {
+            get { return neptunkod; }
+            set
+            {
+                if (value.Length <= 6)
+                {
+                    neptunkod = value;
+                }
+
+            }
+        }
+
+    }
+    public class Szemely
+    {
+        protected string nev;
+        private int kor;
 
         public int Kor
         {
@@ -22,18 +49,22 @@ namespace ConsoleApp2
             {
                 if (value > 0)
                 {
-                    { kor = value; }
-                }
-                else
-                {
-                    Console.WriteLine("Hibás kor!");
+                    kor = value;
                 }
 
             }
         }
+
+        public string Nev
+        {
+            get { return nev; }
+            set { nev = value; }
+        }
+
+
         public override string ToString()
         {
-            return nev + " " + kor + " éves";
+            return $"A tanulo neve {nev} életkora {kor}";
         }
 
     }
@@ -41,13 +72,29 @@ namespace ConsoleApp2
     {
         static void Main(string[] args)
         {
-          Szemely tanulo = new Szemely("Kiss Péter");
-            tanulo.Kor = 21;
-            Console.WriteLine(tanulo.Kor);
-            //Console.WriteLine( tanulo.nev);
-            //Console.WriteLine( tanulo.kor);
+            Szemely tanulo = new Szemely();
+            tanulo.Nev = "Kiss péter";
+            tanulo.Kor = 34;
 
+            Console.WriteLine(tanulo);
+            List<Hallgato> nevsor = new List<Hallgato>();
 
+            for (int i = 0; i < 3; i++)
+            {
+                Hallgato h1 = new Hallgato();
+                Console.WriteLine($"Kérem {i+1}. nevet");
+                h1.Nev = Console.ReadLine();
+                Console.WriteLine($"Kérem {i + 1}. kort");
+                h1.Kor = int.Parse(Console.ReadLine());
+                nevsor.Add(h1);
+            }
+            foreach (var item in nevsor)
+            {
+                Console.WriteLine(item.Nev);
+            }
+
+            //Console.WriteLine(tanulo);
+            //Console.WriteLine(tanulo.kor);
         }
     }
 }
