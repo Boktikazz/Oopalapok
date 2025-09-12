4a193c1a4bb6664346ed79203b35c182aec67094
boktikazz <mezeib@kkszki.hu>
2025-09-12 08:09:26 +0200

    Feladat2 is ready.

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -1,46 +1,35 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+
 
 namespace ConsoleApp2
 {
 
-    public class Szemely 
+     class Szemely 
     {
-        public string Nev { get; set; }
-        private int Eletkor;
 
-        public Szemely(string nev, int eletkor)
+        private string nev;
+        private int kor;
+
+        public Szemely(string Nev, int Kor)
         {
-            Nev = nev;
-            Eletkor = eletkor;
+          nev = Nev;
+          kor = Kor;
+        }
+        public string Kiir()
+        {
+            return $""nev + " " + kor;
         }
 
-        public int Kor
-        {
-            get { return Eletkor; }
-            set
-            {
-                if (value < 0)
-                    throw new ArgumentException("Az életkor nem lehet negatív.");
-                Eletkor = value;
-            }
-        }
-
-        public void Kiir()
-        {
-            Console.WriteLine($"Név: {Nev}, Életkor: {Eletkor}");
-        }
     }
     internal class Program
     {
         static void Main(string[] args)
         {
-            Szemely tanulo = new Szemely("Kiss Péter", 30);
-            tanulo.Kiir();
-            Console.ReadLine();
+          Szemely tanulo = new Szemely("Kiss Péter", 35);
+            Console.WriteLine(tanulo.Kiir());
+            //Console.WriteLine( tanulo.nev);
+            //Console.WriteLine( tanulo.kor);
+
 
         }
     }
